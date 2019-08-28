575c93ea0cfdc71164002f3570b322ffe59d4ed2
pawan13 <pawanshiwakoti13@gmail.com>
2019-08-28 15:46:50 +1000

    Add files via upload
    Contents added on the files.

--- GetInfo.cs
+++ GetInfo.cs
@@ -22,13 +22,58 @@ namespace Assignment2_ChatApp.Droid.Resources.Fragments
 
             // Create your fragment here
         }
+        public class OnGetInfoCompletEventArgs : EventArgs
+        {
+            public string TxtName { get; set; }
+            public int BackgroundColor { get; set; }
+        }
+
+        public event EventHandler<OnGetInfoCompletEventArgs> OnGetInfoComplete;
+
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);View view = inflater.Inflate(Resource.Layout.getinfo, container, false);
+            // return base.OnCreateView(inflater, container, savedInstanceState);
+            View view = inflater.Inflate(Resource.Layout.GetInfo, container, false);
+            EditText txtName = view.FindViewById<EditText>(Resource.Id.txtName);
+            Button btnOkay = view.FindViewById<Button>(Resource.Id.btnOkay);
+            RadioGroup radioGroup = view.FindViewById<RadioGroup>(Resource.Id.radioGroup);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            btnOkay.Click += (o, e) =>
+            {
+                var args = new OnGetInfoCompletEventArgs { TxtName = txtName.Text.Trim() };
+
+                switch (radioGroup.CheckedRadioButtonId)
+                {
+                    case Resource.Id.radioRed:
+                        args.BackgroundColor = 1;
+                        break;
+
+                    case Resource.Id.radioGreen:
+                        args.BackgroundColor = 2;
+                        break;
+
+                    case Resource.Id.radioBlue:
+                        args.BackgroundColor = 3;
+                        break;
+                }
+
+                OnGetInfoComplete.Invoke(this, args);
+
+                Dismiss();
+            };
+
+            return view;
         }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+            base.OnActivityCreated(savedInstanceState);
+        }
+
+       
     }
-}
+    }
--- Main.cs
+++ Main.cs
@@ -5,14 +5,106 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Assignment2_ChatApp.Droid.Resources.Fragments;
+using Microsoft.AspNet.SignalR.Client;
 
 namespace Assignment2_ChatApp.Droid
 {
-    class Main
+    [Activity(Label = "Assignment2_ChatApp.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    class Main:Activity
     {
+        public string UserName;
+        public int BackgroundColor;
+
+        [Obsolete]
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            GetInfo getInfo = new GetInfo();
+            getInfo.OnGetInfoComplete += GetInfo_OnGetInfoComplete;
+            getInfo.Show(FragmentManager, "GetInfo");
+        }
+
+        [Obsolete]
+        private async void GetInfo_OnGetInfoComplete(object sender, GetInfo.OnGetInfoCompletEventArgs e)
+        {
+            UserName = e.TxtName;
+            BackgroundColor = e.BackgroundColor;
+
+            var hubConnection = new HubConnection("http://cforbeginners.com:901");
+            var chatHubProxy = hubConnection.CreateHubProxy("ChatHub");
+
+            chatHubProxy.On<string, int, string>("UpdateChatMessage", (message, color, user) =>
+            {
+                //UpdateChatMessage has been called from server
+
+                RunOnUiThread(() =>
+                {
+                    TextView txt = new TextView(this);
+                    txt.Text = user + ": " + message;
+                    txt.SetTextSize(Android.Util.ComplexUnitType.Sp, 20);
+                    txt.SetPadding(10, 10, 10, 10);
+
+                    switch (color)
+                    {
+                        case 1:
+                            txt.SetTextColor(Color.Red);
+                            break;
+
+                        case 2:
+                            txt.SetTextColor(Color.DarkGreen);
+                            break;
+
+                        case 3:
+                            txt.SetTextColor(Color.Blue);
+                            break;
+
+                        default:
+                            txt.SetTextColor(Color.Black);
+                            break;
+
+                    }
+
+                    txt.LayoutParameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent)
+                    {
+                        TopMargin = 10,
+                        BottomMargin = 10,
+                        LeftMargin = 10,
+                        RightMargin = 10,
+                        Gravity = GravityFlags.Right
+                    };
+
+                    FindViewById<LinearLayout>(Resource.Id.llChatMessages)
+                            .AddView(txt);
+                });
+            });
+
+            try
+            {
+                await hubConnection.Start();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            FindViewById<Button>(Resource.Id.btnSend).Click += async (o, e2) =>
+            {
+                var message = FindViewById<EditText>(Resource.Id.txtChat).Text;
+
+                await chatHubProxy.Invoke("SendMessage", new object[] { message, BackgroundColor, UserName });
+            };
+
+        }
     }
 }
