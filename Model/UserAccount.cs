be5cc3dcff0de5b017fff42c86c4587d52dcaf5a
SCTEE <52845274+SCTEE@users.noreply.github.com>
2019-09-04 18:38:47 +1000

    Add files via upload

---
+++ Model/UserAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Assignment2_ChatApp.Model
+{
+    public class UserAccount
+    {
+        [PrimaryKey]
+        public string ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public UserAccount()
+        {
+
+        }
+    }
+
+    public class FriendAccount
+    {
+        [PrimaryKey]
+        public string ID { get; set; }
+        public string Username { get; set; }
+
+        public FriendAccount()
+        {
+          
+        }
+    }
+
+}
---
+++ Model/UserList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment2_ChatApp.Model
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserList : ContentPage
+    {
+        public UserList()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.MyDatabase.GetAllInfo();
+        }
+    }
+}
