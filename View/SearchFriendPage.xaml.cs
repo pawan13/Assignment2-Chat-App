13f176a28d992c51842cbcbc1617cc575822b51b
SCTEE <52845274+SCTEE@users.noreply.github.com>
2019-09-11 18:56:41 +1000

    Update SearchFriendPage.xaml.cs

--- View/SearchFriendPage.xaml.cs
+++ View/SearchFriendPage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
 using Xamarin.Forms;
+using System.IO;
 using Xamarin.Forms.Xaml;
 
 namespace Assignment2_ChatApp.View
@@ -12,26 +13,49 @@ namespace Assignment2_ChatApp.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SearchFriendPage : ContentPage
     {
-        public SearchFriendPage()
+        Model.UserAccount users = new Model.UserAccount();
+        public SearchFriendPage(Model.UserAccount user)
         {
             InitializeComponent();
+            Showuser(user.Username);
         }
 
+        async void Showuser(String user)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Chattydata.db3");
+            SQLiteAsyncConnection _database = new SQLiteAsyncConnection(path);
+            Model.UserAccount d1 = await _database.Table<Model.UserAccount>().Where(x => x.Username == user).FirstOrDefaultAsync();
+            users = d1;
+        }
+
+        async void SearchbuttonClicked(Object sender, EventArgs e)
+        {
+           List<Model.UserAccount> search = await App.MyDatabase.GetFriendInfo(searchfriendname.Text);
+            if(search.Count > 0)
+            {
+                await Navigation.PushAsync(new ShowSearchResult(users.Username, search));
+            }
+            else
+            {
+                await DisplayAlert("Search Friend", "Friend could not be found", "OK");
+            }
+        }
+               
         async void Go_FriendListPage_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new FriendListPage());
+            await Navigation.PushAsync(new FriendListPage(users));
             //throw new NotImplementedException();
         }
 
         async void Go_SearchFriendPage_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new SearchFriendPage());
+            await Navigation.PushAsync(new SearchFriendPage(users));
             //throw new NotImplementedException();
         }
 
         async void Go_ProfilePage_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new ProfilePage());
+            await Navigation.PushAsync(new ProfilePage(users));
             //throw new NotImplementedException();
         }
     }
