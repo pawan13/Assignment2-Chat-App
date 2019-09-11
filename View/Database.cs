64961886333cb9493cc2107138ba7d09550ad955
SCTEE <52845274+SCTEE@users.noreply.github.com>
2019-09-11 18:47:53 +1000

    Update Database.cs

--- View/Database.cs
+++ View/Database.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using SQLite;
@@ -19,7 +19,13 @@ namespace Assignment2_ChatApp.View
             //line 16 creates the table if it is not exist, else it does nothing.
             _database.CreateTableAsync<Model.UserAccount>().Wait();
 
-            _database.InsertAsync(new Model.UserAccount { ID = "1", Username = " test11", Password = "123" });
+            _database.InsertAsync(new Model.UserAccount { ID = "1", Username = "test11", Password = "123" });
+
+            _database.DropTableAsync<Model.FriendAccount>().Wait();
+            _database.CreateTableAsync<Model.FriendAccount>().Wait();
+
+            _database.InsertAsync(new Model.FriendAccount { ID = "1", Username = "test1", FriendUsername = "test3" });
+            _database.InsertAsync(new Model.FriendAccount { ID = "2", Username = "test2", FriendUsername = "test3" });
             //_database.InsertAsync(new Model.UserAccount { Username = " test11", Password = "123" });
         }
 
@@ -47,8 +53,46 @@ namespace Assignment2_ChatApp.View
             else
             {
                 return "The username already exist";
-  
+
             }
         }
+
+        public Task<List<Model.FriendAccount>> FriendList(Model.UserAccount user)
+        {
+            return _database.Table<Model.FriendAccount>().Where(x => x.Username == user.Username || x.FriendUsername == user.Username).ToListAsync();
+        }
+
+        public Task<List<Model.UserAccount>> GetFriendInfo(string name)
+        {
+            // Model.UserAccount d1 = await _database.Table<Model.UserAccount>().Where(x => x.Username == userName1 && x.Password == pwd1).FirstOrDefaultAsync();
+
+            return _database.Table<Model.UserAccount>().Where(x => x.Username == name).ToListAsync();
+        }
+
+        public async Task SaveFriendInfo(Model.FriendAccount info)
+        {
+            await _database.InsertAsync(info);
+        }
+
+        public async Task RemoveFriendInfo(Model.FriendAccount info)
+        {
+            await _database.DeleteAsync(info);
+        }
+
+        async public Task<string> Addfriend(Model.FriendAccount user)
+        {
+            Model.FriendAccount d1 = await _database.Table<Model.FriendAccount>().Where(x => x.Username == user.Username).FirstOrDefaultAsync();
+            if (d1 == null)
+            {
+                await _database.InsertAsync(user);
+                return "Successfully Added";
+            }
+            else
+            {
+                return "The username already exist";
+
+            }
+        }
+
     }
 }
