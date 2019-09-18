0644cd614de13bb76a1f530d770c7de956893d42
pawan13 <pawanshiwakoti13@gmail.com>
2019-09-18 16:42:27 +1000

    Add files via upload

--- ChatHub.cs
+++ ChatHub.cs
@@ -3,11 +3,28 @@ using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
+    
     public class ChatHub : Hub
     {
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+        public async Task SendMessageGroup(string roomName, string user, string message)
+        {
+            await Clients.Groups(roomName).SendAsync("ReceiveMessage", user, message);
+        }
+
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
     }
 }
